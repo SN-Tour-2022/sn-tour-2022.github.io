5c21f890f6273f6c4b767d093208b5dd1d0f8190
Tim Hilton <tim@timhilton.net>
2022-06-21 02:33:58 -0700

    adds chad and vy to the hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,15 @@
-import {ReactComponent as TourLogo} from '../svgs/tourLogo.svg';
+import {ReactComponent as TourLogo} from "../svgs/tourLogo.svg";
 
 const Hero = () => {
     return (
         <section className="hero">
-            <img className="hero-banner" src="SpyNinjasBanner.jpg" alt=''/>
+            <img className="hero-banner" src="SpyNinjasBanner.jpg" alt=""/>
             <div className="hero-logo-wrapper">
-                <TourLogo className='hero-tour-logo'/>
+                <TourLogo className="hero-tour-logo"/>
+            </div>
+            <div className="hero-chad-vy">
+                <img src="chadfighting.png" alt=""/>
+                <img src="vyfighting.png" alt="" />
             </div>
         </section>
     )
