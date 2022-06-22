7c2b043e51f613cc22580a1c8147a35201e82423
Tim Hilton <tim@timhilton.net>
2022-06-22 10:55:01 -0700

    moves shop to the end of list

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,18 +17,18 @@ const NavBar = () => {
                 <Link to="/" className="logo-link"><Logo className="logo"/></Link>
                 <div className="nav-link-container">
                     <a className="nav-links" href="https://spyninjas.net/" target="_blank" rel="noreferrer">Website</a>
-                    <a className="nav-links" href="https://www.spyninjasstore.com/" target="_blank" rel="noreferrer" onClick={() => setOpen(!open)}>Shop</a>
                     <HashLink className="nav-links" to="/#tour" smooth scroll={el => scrollWidthOffset(el)}>Tour Dates</HashLink>
                     <Link className="nav-links" to="/competition">Competition</Link>
+                    <a className="nav-links" href="https://www.spyninjasstore.com/" target="_blank" rel="noreferrer" onClick={() => setOpen(!open)}>Shop</a>
                 </div>
                 <Hamburger open={open} setOpen={setOpen}/>
             </div>
             <div className={open ? "menu-tray menu-tray-open" : "menu-tray"}>
                 <ul className="container">
                     <li><a className="nav-links" href="https://spyninjas.net/" target="_blank" rel="noreferrer" onClick={() => setOpen(!open)}>Spy Ninjas Website</a></li>
-                    <li><a className="nav-links" href="https://www.spyninjasstore.com/" target="_blank" rel="noreferrer" onClick={() => setOpen(!open)}>Shop</a></li>
                     <li><HashLink className="nav-links" to="/#tour" smooth scroll={el => scrollWidthOffset(el)} onClick={() => setOpen(!open)}>Tour Dates</HashLink></li>
                     <li><Link className="nav-links" to="/competition" onClick={() => setOpen(!open)}>Competition</Link></li>
+                    <li><a className="nav-links" href="https://www.spyninjasstore.com/" target="_blank" rel="noreferrer" onClick={() => setOpen(!open)}>Shop</a></li>
                 </ul>
             </div>
         </nav>
