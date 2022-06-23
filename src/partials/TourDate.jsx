a2054f35bc797f606ff6842fbcc93eadd30b7a8a
Tim Hilton <tim@timhilton.net>
2022-06-22 22:04:00 -0700

    fix ticket 1pm no vip

--- src/partials/TourDate.jsx
+++ src/partials/TourDate.jsx
@@ -10,6 +10,29 @@ const TourDate = ({
     vipSoldOut, 
     vipTicketLink
 }) => {
+    let buttons;
+
+    if (time === '1 PM') {
+        buttons = 
+        <div className="ticket-btns single-btn">
+            <Button
+                type={tixSoldOut ? "red" : "green"}
+                copy={tixSoldOut ? "Sold Out" : "Tickets"}
+                tix={ticketLink} />
+        </div>
+    } else {
+        buttons = 
+        <div className="ticket-btns">
+            <Button
+                type={tixSoldOut ? "red" : "green"}
+                copy={tixSoldOut ? "Sold Out" : "Tickets"}
+                tix={ticketLink} />
+            <Button
+                type={vipSoldOut ? "red" : "yellow"}
+                copy={vipSoldOut ? "Sold Out" : "VIP"}
+                tix={vipTicketLink} />
+        </div>
+    }
     return (
         <div className="tour-date">
             <div className="tour-stop">
@@ -17,16 +40,7 @@ const TourDate = ({
                 <span className="venue">{venue}</span>
             </div>
             <div className="date-time">{date}<br className="sm-hidden"/><span className='sm-show'>&nbsp;&nbsp;|&nbsp;&nbsp;</span>{time}</div>
-            <div className="ticket-btns">
-                <Button
-                    type={tixSoldOut ? "red" : "green"}
-                    copy={tixSoldOut ? "Sold Out" : "Tickets"}
-                    tix={ticketLink} />
-                <Button
-                    type={vipSoldOut ? "red" : "yellow"}
-                    copy={vipSoldOut ? "Sold Out" : "VIP"}
-                    tix={vipTicketLink} />
-            </div>
+            {buttons}
         </div>
     )
 }
